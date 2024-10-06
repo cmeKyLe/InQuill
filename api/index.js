18a691129d0624711b4be19808130c5e75316bd5
cmeKyLe <khant.khantminthaw2021@gmail.com>
2024-10-05 21:21:53 -0700

    installed bcrypt

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 
 
 mongoose.connect('mongodb+srv://khantminthaw64:qZm7gRkkRoVWknk2@cluster0.xyhqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
@@ -10,8 +11,13 @@ app.use(cors());
 app.use(express.json());
 app.post('/register', async (req, res) => {
     const{username, password} = req.body;
-    const userDoc = await User.create({username, password});
-  res.json(userDoc);
+    try{
+        const userDoc = await User.create({username, password});
+        res.json(userDoc);
+    }catch(e){
+        res.status(400).json(e);
+    }
+  
 });
 app.listen(4000);
 
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -6,12 +6,20 @@ export default function RegisterPage() {
     const[password, setPassword] = useState('');
     async function register(ev){
         ev.preventDefault();
-        await fetch('http://localhost:4000/register',{
-            method: 'POST' ,
-            body: JSON.stringify({username, password}),
-            headers: {'Content-Type': 'application/json'},
-        });
+        
+        const response = await fetch('http://localhost:4000/register',{
+                method: 'POST' ,
+                body: JSON.stringify({username, password}),
+                headers: {'Content-Type': 'application/json'},
+            });
+        
+        if(response.status === 200){
+            alert('Registeration successful');
+        }else{
+            alert('Registration failed');
+        }    
     }
+    
     return(
         <form className="Register" onSubmit={register}>
             <h1>Register</h1>
