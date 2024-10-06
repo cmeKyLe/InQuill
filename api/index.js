239ea71e9c8ac8e78ccef36d7a14b931be884d42
cmeKyLe <khant.khantminthaw2021@gmail.com>
2024-10-05 19:07:30 -0700

    save current changes before checkout

--- api/index.js
+++ api/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.post('/register',(req,res)=> {
+
+mongoose.connect('mongodb+srv://InQuill:k5FognQzjE4fiPlL@cluster0.ss2f0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register',async (req,res)=> {
     const {username, password} = req.body;
-    res.json({requestData:{username, password}});
+    try{
+        const userDoc = await User.create({username,password});
+        res.json(userDoc);
+    } catch(e){
+        res.status(400).json(e);
+    }
+    
+    
 });
 
 app.listen(4000);
 
 
-//mongodb+srv://InQuill:k5FognQzjE4fiPlL@cluster0.ss2f0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
