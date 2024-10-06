f4dff12ee76bb60c2685625fe1ac1c89c561b9b5
cmeKyLe <khant.khantminthaw2021@gmail.com>
2024-10-05 21:31:19 -0700

    Revert "installed bcrypt"
    This reverts commit 18a691129d0624711b4be19808130c5e75316bd5.

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
-const bcrypt = require('bcryptjs');
 
 
 mongoose.connect('mongodb+srv://khantminthaw64:qZm7gRkkRoVWknk2@cluster0.xyhqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
@@ -11,13 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.post('/register', async (req, res) => {
     const{username, password} = req.body;
-    try{
-        const userDoc = await User.create({username, password});
-        res.json(userDoc);
-    }catch(e){
-        res.status(400).json(e);
-    }
-  
+    const userDoc = await User.create({username, password});
+  res.json(userDoc);
 });
 app.listen(4000);
 
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -6,20 +6,12 @@ export default function RegisterPage() {
     const[password, setPassword] = useState('');
     async function register(ev){
         ev.preventDefault();
-        
-        const response = await fetch('http://localhost:4000/register',{
-                method: 'POST' ,
-                body: JSON.stringify({username, password}),
-                headers: {'Content-Type': 'application/json'},
-            });
-        
-        if(response.status === 200){
-            alert('Registeration successful');
-        }else{
-            alert('Registration failed');
-        }    
+        await fetch('http://localhost:4000/register',{
+            method: 'POST' ,
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type': 'application/json'},
+        });
     }
-    
     return(
         <form className="Register" onSubmit={register}>
             <h1>Register</h1>
